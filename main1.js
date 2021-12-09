f238e35fa1ed4ba4554580933631108ac7e7ca3a
hoale0507 <hoale090293@gmail.com>
2021-12-09 10:45:03 +0700

    add file

---
+++ main1.js
@@ -0,0 +1,10 @@
+let physic = 10;
+let chemic = 9;
+let bio = 8;
+let average = (physic + chemic + bio) / 3;
+document.write ("Average is" + average);
+let sum = (physic + chemic + bio);
+document.write("Sum is " + sum);
+console.log(average);
+console.log(sum);
+
